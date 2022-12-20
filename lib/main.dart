9ecec5cd064b353d6b6e62f5a04a62b39a1c4b31
Amansrivastava534 <Amansrivastava534@gmail.com>
2022-12-20 22:27:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:attendance_app/homescreen.dart';
+import 'package:attendance_app/loginscreen.dart';
+import 'package:attendance_app/model/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:month_year_picker/month_year_picker.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Attendance App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: KeyboardVisibilityProvider(
+        child: AuthCheck(),
+      ),
+      localizationsDelegates: const [
+        MonthYearPickerLocalizations.delegate,
+      ],
+    );
+  }
+}
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  bool userAvailable = false;
+  late SharedPreferences sharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getCurrentUser();
+  }
+
+  void _getCurrentUser() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    try {
+      if(sharedPreferences.getString('employeeId') != null) {
+        setState(() {
+          User.employeeId = sharedPreferences.getString('employeeId')!;
+          userAvailable = true;
+        });
+      }
+    } catch(e) {
+      setState(() {
+        userAvailable = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return userAvailable ? const HomeScreen() : const LoginScreen();
+  }
+}
+
